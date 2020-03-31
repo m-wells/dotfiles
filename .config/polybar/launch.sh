e8bfa8cc548a1b23d4ba00b738b4150a99772dc2
Mark Wells <mwellsa@gmail.com>
2020-03-31 11:15:38 -0400

    unlink

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,1 +1,17 @@
-/home/mark/Dotfiles/HOME/.config/polybar/launch.sh
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# polybar top &
+
+# Launch Polybar on each monitor, using default config location ~/.config/polybar/config
+for i in $(polybar -m | awk -F: '{print $1}'); do 
+    MONITOR=$i polybar top &
+done
+
+echo "Polybar launched..."
