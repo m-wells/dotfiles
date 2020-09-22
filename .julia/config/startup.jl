0939c6562426498cefdafa5767ee4e8351914229
m-wells <mwellsa@gmail.com>
2020-09-22 11:22:56 -0400

    julia 1.5 revise

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,30 +1,16 @@
-atreplinit() do repl
-    try
-        sleep(0.1)
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-    catch e
-        @warn(e.msg)
-    end
-
-    try
-        @eval using REPL
-        @eval using REPL: LineEdit
-
-        repl.interface = REPL.setup_interface(repl)
-        repl.interface.modes[1].on_enter = function (s)
-            mktemp() do path, io
-                input = chomp(String(take!(copy(LineEdit.buffer(s)))))
-                if isempty(input)
-                    run(`vim -c "set filetype=julia" $(path)`)
-                    sleep(0.1)
-                    input = read(io, String)
-                    write(LineEdit.buffer(s), chomp(input))
-                end
-                ast = Base.parse_input_line(String(take!(copy(LineEdit.buffer(s)))), depwarn=false)
-                return !(isa(ast, Expr) && ast.head === :incomplete)
-            end
+if Base.VERSION < v"1.5"
+    atreplinit() do repl
+        try
+            sleep(0.1)
+            @eval using Revise
+            @async Revise.wait_steal_repl_backend()
+        catch e
+            @warn(e.msg)
         end
+    end
+else
+    try
+        using Revise
     catch e
         @warn(e)
     end
